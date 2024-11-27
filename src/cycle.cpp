82455bca3281cbbbe6570cf305a7e3d390460977
Max Gonzalez Saez-Diez <saezdiez@davisson.princeton.edu>
2024-11-27 10:50:25 -0500

    breaking cycle.cpp into better code.

--- src/cycle.cpp
+++ src/cycle.cpp
@@ -12,16 +12,15 @@ static Emulator *emulator = nullptr;
 static Cache *iCache = nullptr;
 static Cache *dCache = nullptr;
 static std::string output;
-static uint32_t cycleCount;
 static Emulator::InstructionInfo info;
 static uint32_t IF_DELAY;
-static uint32_t currIF;
 static PipeState pipeState;
 static bool HALTING; // is the halting instruction flowing thorugh the pipeline?
 
 // enum for specific instructions (i.e. halting)
-enum Instructions {
-    HALT_INSTRUCTION = 0xfeedfeed
+enum Instructions
+{
+    HALT_INSTRUCTION = 0xfeedfeed,
 };
 
 // NOTE: The list of places in the source code that are marked ToDo might not be comprehensive.
@@ -65,6 +64,42 @@ void stall_IF_stage(PipeState &pipline)
     pipline.idInstr = 0;
 }
 
+enum ControlSignal
+{
+    HALT_INSTRUCT_IN_PIPELINE = 0,
+    FETCH_NEW = 100,
+    IF_CACHE_MISS = 200,
+};
+
+// Control Detection Unit: returns pipeline behavior
+uint32_t Control(PipeState &pipline)
+{
+    uint32_t currentCycle = emulator->getCurCyle();
+    pipline.cycle = currentCycle;
+    if (info.isHalt) {
+        return HALT_INSTRUCT_IN_PIPELINE;
+    } else if (IF_DELAY == 0 && !HALTING)
+    {
+        return FETCH_NEW;
+    } else if (IF_DELAY > 0 && !HALTING) {
+        IF_DELAY -= 1;
+        return IF_CACHE_MISS;
+    }
+
+    if (false)
+    {
+        // handle dCache delays (in a multicycle style)
+        uint32_t cacheDelay;
+        if (info.isValid && (info.opcode == OP_LBU || info.opcode == OP_LHU || info.opcode == OP_LW))
+            cacheDelay += dCache->access(info.address, CACHE_READ) ? 0 : dCache->config.missLatency;
+
+        if (info.isValid && (info.opcode == OP_SB || info.opcode == OP_SH || info.opcode == OP_SW))
+            cacheDelay += dCache->access(info.address, CACHE_WRITE) ? 0 : dCache->config.missLatency;
+    }
+
+    return 0;
+}
+
 // Run the emulator for a certain number of cycles
 // return HALT if the simulator halts on 0xfeedfeed
 // return SUCCESS if we have executed the desired number of cycles
@@ -75,47 +110,25 @@ Status runCycles(uint32_t cycles)
 
     while (cycles == 0 || count < cycles)
     {
-        uint32_t currentCycle = emulator->getCurCyle();
-        if (currentCycle == cycleCount && !HALTING)
+        uint32_t state = Control(pipeState);
+        if (state == FETCH_NEW)
         {
             // here move all instructions one forward
             moveAllForward(pipeState);
 
             info = emulator->executeInstruction();
-            pipeState.cycle = currentCycle; // get the execution cycle count
             pipeState.ifInstr = info.instruction;
 
-            uint32_t cacheDelay = 0; // initially no delay for cache read/write
-
             // handle iCache delay
-            currIF = 0;
             IF_DELAY = iCache->access(info.pc, CACHE_READ) ? 0 : iCache->config.missLatency;
-
-            // handle dCache delays (in a multicycle style)
-            if (info.isValid && (info.opcode == OP_LBU || info.opcode == OP_LHU || info.opcode == OP_LW))
-                cacheDelay += dCache->access(info.address, CACHE_READ) ? 0 : dCache->config.missLatency;
-
-            if (info.isValid && (info.opcode == OP_SB || info.opcode == OP_SH || info.opcode == OP_SW))
-                cacheDelay += dCache->access(info.address, CACHE_WRITE) ? 0 : dCache->config.missLatency;
-
-            cycleCount += 1 + cacheDelay + IF_DELAY;
-
-            if (info.isHalt)
-            {
-                HALTING = true;
-            }
         }
-        else
-        {
-            if (HALTING) {
-                moveAllForward(pipeState);
-            } else {
-                stall_IF_stage(pipeState);
-            }
+        else if (state == IF_CACHE_MISS) {
+            stall_IF_stage(pipeState);
+        } else if (state == HALT_INSTRUCT_IN_PIPELINE) {
+            moveAllForward(pipeState);
         }
 
         count += 1;
-        pipeState.cycle = emulator->getCurCyle();
         emulator->ranCyle();
 
         if (pipeState.wbInstr == HALT_INSTRUCTION)
@@ -125,7 +138,6 @@ Status runCycles(uint32_t cycles)
         }
     }
 
-    
     dumpPipeState(pipeState, output);
     return status;
 }
@@ -148,7 +160,7 @@ Status runTillHalt()
 Status finalizeSimulator()
 {
     emulator->dumpRegMem(output);
-    SimulationStats stats{emulator->getDin(), cycleCount, iCache->getHits(), iCache->getMisses(), dCache->getHits(), dCache->getMisses()}; // TODO: Incomplete Implementation
+    SimulationStats stats{emulator->getDin(), emulator->getCurCyle(), iCache->getHits(), iCache->getMisses(), dCache->getHits(), dCache->getMisses()}; // TODO: Incomplete Implementation
     dumpSimStats(stats, output);
     return SUCCESS;
 }
