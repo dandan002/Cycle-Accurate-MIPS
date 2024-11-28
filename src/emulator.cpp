962c733d3c60469c2a6e6e38b12c86be2484689a
Max Gonzalez Saez-Diez <saezdiez@compton.princeton.edu>
2024-11-28 12:32:42 -0500

    DONE: updated spacing, brackets for merge

--- src/emulator.cpp
+++ src/emulator.cpp
@@ -10,7 +10,8 @@
 #include <iostream>
 using namespace std;
 
-Emulator::Emulator() {
+Emulator::Emulator()
+{
     // Initialize member variables
     memory = nullptr;
     PC = 0;
@@ -20,12 +21,15 @@ Emulator::Emulator() {
     din = 0;
 }
 
-Emulator::~Emulator() {
-    if (memory) delete memory;
+Emulator::~Emulator()
+{
+    if (memory)
+        delete memory;
 }
 
 // extract specific bits [start, end] from a 32 bit instruction
-uint Emulator::extractBits(uint32_t instruction, int start, int end) {
+uint Emulator::extractBits(uint32_t instruction, int start, int end)
+{
     int bitsToExtract = start - end + 1;
     uint32_t mask = (1 << bitsToExtract) - 1;
     uint32_t clipped = instruction >> end;
@@ -33,40 +37,47 @@ uint Emulator::extractBits(uint32_t instruction, int start, int end) {
 }
 
 // sign extend smol to a 32 bit unsigned int
-uint32_t Emulator::signExt(uint16_t smol) {
+uint32_t Emulator::signExt(uint16_t smol)
+{
     uint32_t x = smol;
     uint32_t extension = 0xffff0000;
     return (smol & 0x8000) ? x ^ extension : x;
 }
 
-bool Emulator::checkOverflow(uint32_t val1, uint32_t val2) {
+bool Emulator::checkOverflow(uint32_t val1, uint32_t val2)
+{
     int64_t result = int64_t(val1) + int64_t(val2);
     // use UINT32_MAX here as the max value is uint32. I think we can prob get rid of the <0 but not sure. will leave for now cause it feels better LOL
-    if (result > UINT32_MAX || result < 0) {
+    if (result > UINT32_MAX || result < 0)
+    {
         return true;
     }
     return false;
 }
 
-bool Emulator::checkOverflowSigned(uint32_t val1, int32_t val2) {
-    int64_t result = int64_t(val1) + int64_t(val2); 
-    if (result > UINT32_MAX || result < INT32_MIN) {
-        return true; 
+bool Emulator::checkOverflowSigned(uint32_t val1, int32_t val2)
+{
+    int64_t result = int64_t(val1) + int64_t(val2);
+    if (result > UINT32_MAX || result < INT32_MIN)
+    {
+        return true;
     }
-    return false; 
+    return false;
 }
 
 // dump registers and memory
-void Emulator::dumpRegMem(const std::string& output_name) {
+void Emulator::dumpRegMem(const std::string &output_name)
+{
     assert(memory);
     dumpRegisterState(regData.reg, output_name);
     dumpMemoryState(memory, output_name);
 }
 
-Emulator::InstructionInfo Emulator::executeInstruction() {
+Emulator::InstructionInfo Emulator::executeInstruction()
+{
     assert(memory);
-    InstructionInfo info;  // information struct for this instruction
-    info.pc = PC;          // fill PC before its updated
+    InstructionInfo info; // information struct for this instruction
+    info.pc = PC;         // fill PC before its updated
 
     uint32_t instruction;
     memory->getMemValue(PC, instruction, WORD_SIZE);
@@ -76,7 +87,8 @@ Emulator::InstructionInfo Emulator::executeInstruction() {
     info.nextPC = (encounteredBranch) ? savedBranch : PC + 4;
     if (!encounteredBranch)
         PC += 4;
-    else {
+    else
+    {
         PC = savedBranch;
         encounteredBranch = false;
     }
@@ -87,7 +99,8 @@ Emulator::InstructionInfo Emulator::executeInstruction() {
 
     // check for halt instruction and return immediately
     info.isHalt = (instruction == 0xfeedfeed);
-    if (instruction == 0xfeedfeed) {
+    if (instruction == 0xfeedfeed)
+    {
         return info;
     }
 
@@ -105,7 +118,7 @@ Emulator::InstructionInfo Emulator::executeInstruction() {
     uint32_t zeroExtImm = immediate;
 
     uint32_t branchAddr = signExtImm << 2;
-    uint32_t jumpAddr = (PC & 0xf0000000) ^ (address << 2);  // assumes PC += 4 just happened
+    uint32_t jumpAddr = (PC & 0xf0000000) ^ (address << 2); // assumes PC += 4 just happened
 
     // fill the bitfields in the instruction info struct
     info.opcode = opcode;
@@ -122,173 +135,192 @@ Emulator::InstructionInfo Emulator::executeInstruction() {
     info.jumpAddr = jumpAddr;
 
     uint32_t a = 0;
-    uint32_t b = 0;  
+    uint32_t b = 0;
     int32_t c = 0;
 
-    switch (opcode) {
-        case OP_ZERO:  // R-type instruction
-            switch (funct) {
-                case FUN_ADD:
-                    a = regData.registers[rs];
-                    b = regData.registers[rt];
-                    
-                    if (checkOverflow(a, b)) {
-                        info.isOverflow = true;
-                    } else {
-                        regData.registers[rd] = a + b;
-                    }
-                    break;
-                case FUN_ADDU:
-                    a = regData.registers[rs];
-                    b = regData.registers[rt];
-
-                    if (checkOverflow(a, b)) {
-                        info.isOverflow = true;
-                    } else {
-                        regData.registers[rd] = a + b;
-                    }
-                    break;
-                case FUN_AND:
-                    regData.registers[rd] = regData.registers[rs] & regData.registers[rt];
-                    break;
-                case FUN_JR:
-                    encounteredBranch = true;
-                    savedBranch = regData.registers[rs];
-                    break;
-                case FUN_NOR:
-                    regData.registers[rd] = ~(regData.registers[rs] | regData.registers[rt]);
-                    break;
-                case FUN_OR:
-                    regData.registers[rd] = regData.registers[rs] | regData.registers[rt];
-                    break;
-                case FUN_SLT:
-                    regData.registers[rd] =
-                        (int32_t(regData.registers[rs]) < int32_t(regData.registers[rt])) ? 1 : 0;
-                    break;
-                case FUN_SLTU:
-                    regData.registers[rd] = (regData.registers[rs] < regData.registers[rt]) ? 1 : 0;
-                    break;
-                case FUN_SLL:
-                    regData.registers[rd] = regData.registers[rt] << shamt;
-                    break;
-                case FUN_SRL:
-                    regData.registers[rd] = regData.registers[rt] >> shamt;
-                    break;
-                case FUN_SUB:
-                    regData.registers[rd] = regData.registers[rs] - regData.registers[rt];
-                    break;
-                case FUN_SUBU:
-                    regData.registers[rd] = regData.registers[rs] - regData.registers[rt];
-                    break;
-                default:
-                    std::cerr << LOG_ERROR << "Illegal operation..." << std::endl;
-                    info.isValid = false;
-            }
-            break;
-
-        case OP_ADDI:
+    switch (opcode)
+    {
+    case OP_ZERO: // R-type instruction
+        switch (funct)
+        {
+        case FUN_ADD:
             a = regData.registers[rs];
-            c = signExtImm;
-            if (checkOverflowSigned(a, c)) {
+            b = regData.registers[rt];
+
+            if (checkOverflow(a, b))
+            {
                 info.isOverflow = true;
-            } else {
-                regData.registers[rt] = a + c;
+            }
+            else
+            {
+                regData.registers[rd] = a + b;
             }
             break;
-        case OP_ADDIU:
+        case FUN_ADDU:
             a = regData.registers[rs];
-            c = signExtImm;
-            if (checkOverflowSigned(a, c)) {
+            b = regData.registers[rt];
+
+            if (checkOverflow(a, b))
+            {
                 info.isOverflow = true;
-            } else {
-                regData.registers[rt] = a + c;
+            }
+            else
+            {
+                regData.registers[rd] = a + b;
             }
             break;
-        case OP_ANDI:
-            regData.registers[rt] = regData.registers[rs] & zeroExtImm;
+        case FUN_AND:
+            regData.registers[rd] = regData.registers[rs] & regData.registers[rt];
             break;
-        case OP_BEQ:
-            if (regData.registers[rs] == regData.registers[rt]) {
-                encounteredBranch = true;
-                savedBranch = PC + branchAddr;
-            }
-            break;
-        case OP_BNE:
-            if (regData.registers[rs] != regData.registers[rt]) {
-                encounteredBranch = true;
-                savedBranch = PC + branchAddr;
-            }
-            break;
-        case OP_BLEZ:
-            if (regData.registers[rs] == 0 || (regData.registers[rs] & 0x80000000)) {
-                encounteredBranch = true;
-                savedBranch = PC + branchAddr;
-            }
-            break;
-        case OP_BGTZ:
-            if (!(regData.registers[rs] & 0x80000000) && regData.registers[rs] != 0) {
-                encounteredBranch = true;
-                savedBranch = PC + branchAddr;
-            }
-            break;
-        case OP_J:
+        case FUN_JR:
             encounteredBranch = true;
-            savedBranch = jumpAddr;
+            savedBranch = regData.registers[rs];
             break;
-        case OP_JAL:
-            encounteredBranch = true;
-            regData.registers[31] = PC + 4;
-            savedBranch = jumpAddr;
+        case FUN_NOR:
+            regData.registers[rd] = ~(regData.registers[rs] | regData.registers[rt]);
             break;
-        case OP_LBU:
-            info.loadAddress = regData.registers[rs] + signExtImm;  // capture load address
-            memory->getMemValue(regData.registers[rs] + signExtImm, regData.registers[rt],
-                                BYTE_SIZE);
+        case FUN_OR:
+            regData.registers[rd] = regData.registers[rs] | regData.registers[rt];
             break;
-        case OP_LHU:
-            info.loadAddress = regData.registers[rs] + signExtImm;  // capture load address
-            memory->getMemValue(regData.registers[rs] + signExtImm, regData.registers[rt],
-                                HALF_SIZE);
+        case FUN_SLT:
+            regData.registers[rd] =
+                (int32_t(regData.registers[rs]) < int32_t(regData.registers[rt])) ? 1 : 0;
             break;
-        case OP_LUI:
-            regData.registers[rt] = zeroExtImm << 16;
+        case FUN_SLTU:
+            regData.registers[rd] = (regData.registers[rs] < regData.registers[rt]) ? 1 : 0;
             break;
-        case OP_LW:
-            info.loadAddress = regData.registers[rs] + signExtImm;  // capture load address
-            memory->getMemValue(regData.registers[rs] + signExtImm, regData.registers[rt],
-                                WORD_SIZE);
+        case FUN_SLL:
+            regData.registers[rd] = regData.registers[rt] << shamt;
             break;
-        case OP_ORI:
-            regData.registers[rt] = regData.registers[rs] | zeroExtImm;
+        case FUN_SRL:
+            regData.registers[rd] = regData.registers[rt] >> shamt;
             break;
-        case OP_SLTI:
-            regData.registers[rt] = (int32_t(regData.registers[rs]) < int32_t(signExtImm)) ? 1 : 0;
+        case FUN_SUB:
+            regData.registers[rd] = regData.registers[rs] - regData.registers[rt];
             break;
-        case OP_SLTIU:
-            regData.registers[rt] = (regData.registers[rs] < uint32_t(signExtImm)) ? 1 : 0;
-            break;
-        case OP_SB:
-            info.storeAddress = regData.registers[rs] + signExtImm;  // capture store address
-            memory->setMemValue(regData.registers[rs] + signExtImm,
-                                extractBits(regData.registers[rt], 7, 0), BYTE_SIZE);
-            break;
-        case OP_SH:
-            info.storeAddress = regData.registers[rs] + signExtImm;  // capture store address
-            memory->setMemValue(regData.registers[rs] + signExtImm,
-                                extractBits(regData.registers[rt], 15, 0), HALF_SIZE);
-            break;
-        case OP_SW:
-            info.storeAddress = regData.registers[rs] + signExtImm;  // capture store address
-            memory->setMemValue(regData.registers[rs] + signExtImm, regData.registers[rt],
-                                WORD_SIZE);
+        case FUN_SUBU:
+            regData.registers[rd] = regData.registers[rs] - regData.registers[rt];
             break;
         default:
             std::cerr << LOG_ERROR << "Illegal operation..." << std::endl;
             info.isValid = false;
+        }
+        break;
+
+    case OP_ADDI:
+        a = regData.registers[rs];
+        c = signExtImm;
+        if (checkOverflowSigned(a, c))
+        {
+            info.isOverflow = true;
+        }
+        else
+        {
+            regData.registers[rt] = a + c;
+        }
+        break;
+    case OP_ADDIU:
+        a = regData.registers[rs];
+        c = signExtImm;
+        if (checkOverflowSigned(a, c))
+        {
+            info.isOverflow = true;
+        }
+        else
+        {
+            regData.registers[rt] = a + c;
+        }
+        break;
+    case OP_ANDI:
+        regData.registers[rt] = regData.registers[rs] & zeroExtImm;
+        break;
+    case OP_BEQ:
+        if (regData.registers[rs] == regData.registers[rt])
+        {
+            encounteredBranch = true;
+            savedBranch = PC + branchAddr;
+        }
+        break;
+    case OP_BNE:
+        if (regData.registers[rs] != regData.registers[rt])
+        {
+            encounteredBranch = true;
+            savedBranch = PC + branchAddr;
+        }
+        break;
+    case OP_BLEZ:
+        if (regData.registers[rs] == 0 || (regData.registers[rs] & 0x80000000))
+        {
+            encounteredBranch = true;
+            savedBranch = PC + branchAddr;
+        }
+        break;
+    case OP_BGTZ:
+        if (!(regData.registers[rs] & 0x80000000) && regData.registers[rs] != 0)
+        {
+            encounteredBranch = true;
+            savedBranch = PC + branchAddr;
+        }
+        break;
+    case OP_J:
+        encounteredBranch = true;
+        savedBranch = jumpAddr;
+        break;
+    case OP_JAL:
+        encounteredBranch = true;
+        regData.registers[31] = PC + 4;
+        savedBranch = jumpAddr;
+        break;
+    case OP_LBU:
+        info.loadAddress = regData.registers[rs] + signExtImm; // capture load address
+        memory->getMemValue(regData.registers[rs] + signExtImm, regData.registers[rt],
+                            BYTE_SIZE);
+        break;
+    case OP_LHU:
+        info.loadAddress = regData.registers[rs] + signExtImm; // capture load address
+        memory->getMemValue(regData.registers[rs] + signExtImm, regData.registers[rt],
+                            HALF_SIZE);
+        break;
+    case OP_LUI:
+        regData.registers[rt] = zeroExtImm << 16;
+        break;
+    case OP_LW:
+        info.loadAddress = regData.registers[rs] + signExtImm; // capture load address
+        memory->getMemValue(regData.registers[rs] + signExtImm, regData.registers[rt],
+                            WORD_SIZE);
+        break;
+    case OP_ORI:
+        regData.registers[rt] = regData.registers[rs] | zeroExtImm;
+        break;
+    case OP_SLTI:
+        regData.registers[rt] = (int32_t(regData.registers[rs]) < int32_t(signExtImm)) ? 1 : 0;
+        break;
+    case OP_SLTIU:
+        regData.registers[rt] = (regData.registers[rs] < uint32_t(signExtImm)) ? 1 : 0;
+        break;
+    case OP_SB:
+        info.storeAddress = regData.registers[rs] + signExtImm; // capture store address
+        memory->setMemValue(regData.registers[rs] + signExtImm,
+                            extractBits(regData.registers[rt], 7, 0), BYTE_SIZE);
+        break;
+    case OP_SH:
+        info.storeAddress = regData.registers[rs] + signExtImm; // capture store address
+        memory->setMemValue(regData.registers[rs] + signExtImm,
+                            extractBits(regData.registers[rt], 15, 0), HALF_SIZE);
+        break;
+    case OP_SW:
+        info.storeAddress = regData.registers[rs] + signExtImm; // capture store address
+        memory->setMemValue(regData.registers[rs] + signExtImm, regData.registers[rt],
+                            WORD_SIZE);
+        break;
+    default:
+        std::cerr << LOG_ERROR << "Illegal operation..." << std::endl;
+        info.isValid = false;
     }
 
-    if (info.isOverflow || !info.isValid) {
+    if (info.isOverflow || !info.isValid)
+    {
         PC = 0x8000;
     }
-    return info;  // return the InstructionInfo struct of the instruction just executed
+    return info; // return the InstructionInfo struct of the instruction just executed
 }
